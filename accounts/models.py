48cf84c81077731ff3d767f26117d3571eea89f7
azharmehdi <azharmehdi35@gmail.com>
2025-02-09 14:30:00 +0500

    Restructured

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class RegisterUser(models.Model):
+    first_name = models.CharField(max_length=36)
+    middle_name = models.CharField(max_length=36)
+    last_name = 
