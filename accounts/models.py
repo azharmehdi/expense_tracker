b6b945a1c590353e2141f0b430949865475c00ec
azharmehdi <azharmehdi35@gmail.com>
2025-02-10 14:30:00 +0500

    Restructured

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class RegisterUser(models.Model):
+class RegisterUser(AbstractUser):
     first_name = models.CharField(max_length=36)
     middle_name = models.CharField(max_length=36)
     last_name = models.CharField(max_length=36)
     password =models.CharField()
     username =  models.CharField(unique=True)
-    e_mail = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
