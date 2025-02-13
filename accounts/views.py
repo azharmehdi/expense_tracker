65e364453c96ec0ca2782aabcf806b272fd8be82
azharmehdi <azharmehdi35@gmail.com>
2025-02-13 14:30:00 +0500

    Restructured

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators  import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate
 from .serializers import UserRegister
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['POST'])
 def register_user(request):
@@ -11,8 +13,28 @@ def register_user(request):
         Response ({
             "message": "User registered successfully",
             "user": serializer.data
-        }), status =status.HTTP_201_created
+        }, status =status.HTTP_201_created)
 
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    user = authenticate(username=username,password=password)
+    if user is None:
+        return Response ({
+            "error":"invalid credentials"
+        }, status.HTTP_4001_Unauthorized)
+    
+    
+    if user.is_authenticated():
+        refresh = RefreshToken.for_user(user)
+    
+    return Response ({
+        "message":str(refresh),
+        "access":str(refresh.access_token)
+    },status = status.HTTP_201_OK)
 
