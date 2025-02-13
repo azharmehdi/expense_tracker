1fe850fc871af7a84fb75a4e7434595b5cc86de5
azharmehdi <azharmehdi35@gmail.com>
2025-02-13 14:30:00 +0500

    Restructured

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import RegisterUser
+from .models import RegisterUser as User
 
 
 class UserRegister(serializers.ModelSerializer):
     class Meta:
-        model = RegisterUser
+        model = User
         fields = ['first_name','middle_name','last_name','email','password','username']
 
     def validate_first_name(self,value):
@@ -21,3 +21,14 @@ class UserRegister(serializers.ModelSerializer):
         if not value.isalpha():
             raise serializers.ValidationError('Last name must contain only letters')
         return value
+    
+    def validate_password(self,value):
+        if value.len() <=7:
+            raise serializers.ValidationError('Password must be at least 8 characters long')
+        return value
+    
+    def create(self,validated_data):
+        user =User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
